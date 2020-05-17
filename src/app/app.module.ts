1c7feb5c836e3bdfc9271d7c218f1d87e1ede284
Carol Vasconcelos <cvsouza04@gmail.com>
2020-05-17 19:00:24 -0300

    Primeira implementação da comunicação child to child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
 import { FirstChildComponent } from './first-child/first-child.component';
+import { SecondChildComponent } from './second-child/second-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParentComponent,
-    FirstChildComponent
+    FirstChildComponent,
+    SecondChildComponent
   ],
   imports: [
     BrowserModule,
