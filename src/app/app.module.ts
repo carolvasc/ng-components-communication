304e58a83894ac6bdfe761272c1f94c39215b9cf
Carol Vasconcelos <cvsouza04@gmail.com>
2020-05-17 16:26:49 -0300

    Criação dos componentes parent e first-child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { FirstChildComponent } from './first-child/first-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    FirstChildComponent
   ],
   imports: [
     BrowserModule
